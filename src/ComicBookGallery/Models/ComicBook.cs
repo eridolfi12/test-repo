37d43073f24c7314a28560911ceed3fc4fb1bf84
Eric <Eric@ERIC-PC>
2017-11-28 04:48:14 -0500

    Updated comic book detail view layout

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -27,7 +27,7 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                return "/images/" + SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
         }
     }
